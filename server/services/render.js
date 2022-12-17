ead2b3685153678477e9748846e004d2234dc8e6
pr_rockz <priyanshuranjan2806@gmail.com>
2022-12-17 18:42:06 +0530

    Updation and Deletion function are now working.

--- server/services/render.js
+++ server/services/render.js
@@ -5,7 +5,9 @@ exports.homeRoutes = (req, res) => {
     // Make a get request to /api/users
     axios.get('http://localhost:3000/api/users')
         .then(function (response) {
-            res.render('index', { users: response.data });
+            res.render('index', {
+                users: response.data
+            });
         })
         .catch(err => {
             res.send(err);
@@ -19,7 +21,9 @@ exports.add_user = (req, res) => {
 }
 
 exports.update_user = (req, res) => {
-    axios.get('http://localhost:3000/api/users', { params: { id: req.query.id } })
+    axios.get('http://localhost:3000/api/users', {
+        params: { id: req.query.id }
+    })
         .then(function (userdata) {
             res.render("update_user", { user: userdata.data })
         })
